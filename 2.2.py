643e9b8958528396d4fc00967ad0250a427ea5f1
jeis <35922218+arunjyothishk@users.noreply.github.com>
2020-12-13 15:33:32 +0530

    SC files only- deleted all other files,

--- 2.2.py
+++ 2.2.py
@@ -1,3 +1,17 @@
-import numpy
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  4 08:39:29 2020
 
-print(numpy.pow(2,3))
+@author: arunjyothish
+"""
+
+from numpy import *
+angle=int(input("Enter an angle to find it's trignometric values: "))
+print(sin(angle))
+realp=int(input("Enter Real part of an imaginary number: "))
+imagp=int(input("Enter Imaginary part of an imaginary number: "))
+complex_num=complex(realp,imagp)
+print("your Entered imaginary number: ",complex_num)
+print("Its real part: ",real(complex_num))
+print("Its real part: ",imag(complex_num))
+
