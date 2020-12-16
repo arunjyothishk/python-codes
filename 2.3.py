abbbd0ba3ca853d7d2d0d2e02434bae0df6e19ef
jeis <35922218+arunjyothishk@users.noreply.github.com>
2020-12-16 20:33:11 +0530

    functioning vector/classical process time computing

--- 2.3.py
+++ 2.3.py
@@ -6,28 +6,32 @@ Created on Wed Dec  16 10:39:29 2020
 """
 from numpy import *
 import time
+
 arr1=arange(0,1000) # assuming as a row matrix
 # print(arr1)
 arr2=arange(1000,2000) # assuming as a coloum matrix
 
+length=len(arr1)
+
 def classic_method(arr1,arr2):
-    result=zeros(len(arr1),len(arr2)) # initializing result matrix (row*coloum)
-    for i in range(len(arr1)):  #row loop
-        for j in range(len(arr2)):
+    result=zeros((length,length)) # initializing result matrix (row*coloum)
+    for i in range(length):  #row loop
+        for j in range(length):
             result[i][j]=arr1[i]*arr2[j]
     return result
 
 def vectorization_method(arr1,arr2):
-    result=arr1+arr2
+    result=outer(arr1,arr2)
     return result
 
 time_init=time.time()
-classic_method(arr1,arr2)
+print(classic_method(arr1,arr2))
 time_end=time.time()
-print("Process time classical method: ",time.end-time.init)
+print()
+print("Process time classical method: ",(time_end-time_init)*1000,"ms")
 print()
 
 time_init=time.time()
-vectorization_method(arr1,arr2)
+print(vectorization_method(arr1,arr2))
 time_end=time.time()
-print("Process time vectorization method: ",time.end-time.init)
+print("Process time vectorization method: ",1000*(time_end-time_init),"ms")
